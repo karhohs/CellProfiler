2a961b1a706dd57672eaf752643fd2264cb0d595
karhohs <gandalfisarockstar@gmail.com>
2017-02-13 10:07:35 -0500

    updates to adaptivelocalnormalization and seeding. focus on data types.

--- cellprofiler/modules/adaptivelocalnormalization.py
+++ cellprofiler/modules/adaptivelocalnormalization.py
@@ -2,74 +2,117 @@ import cellprofiler.image
 import cellprofiler.module
 import cellprofiler.setting
 import numpy
+import skimage
 import skimage.io
 import skimage.morphology
 import skimage.util
+import scipy.ndimage.filters
+import skimage.exposure
+
 
 class AdaptiveLocalNormalization(cellprofiler.module.ImageProcessing):
+    """
+    Things to keep in mind: There is a fast version and a slow version of normalization. The slow version is true to
+    the algorithm put forth in the paper. However, using disc structuring elements with scipy generic filtering is
+    time intensive. An alternative is to use scipy uniform filtering. It is much faster, but it only works in
+    rectangular windows. The trade-off for speed can be useful, so both slow and fast have been included.
+
+    The normalization produces both positive and negative float values. It is preferred to use uint16 ndarrays,
+    so after calculation of the image it is scaled to fit within the uint16 format. This might introduce some
+    unwanted variation if stitching images or processing a group of images, because each is scaled independently.
+    However, if the distribution of intensities across images is similar then this shouldn't too costly. To globally
+    scale a set of images would require more coding overhead...
+    """
     module_name = "AdaptiveLocalNormalization"
     variable_revision_number = 1
 
-    def aln_normalize(self, img, pixel_location, radius, T_StD):
-        """
-        :param self: an instance of AdaptiveLocalNormalization class
-        :param img: a 2D ndarray/image
-        :param pixel_location: a tuple with 2 elements (row, col), coordinates in an ndarray
-        :param radius: radius of a disc structuring element that defines neighborhood of normalization
-        :param T_StD: the standard deviation threshold. A fraction of the image std.
-        :return I_N: The normalized value at the pixel_location
-        """
-        xy_region, pad_width = self.xy_region_and_pad_width(img.shape, pixel_location, radius)
-        if pad_width > 0:
-            img_pad = skimage.util.pad(img, pad_width, "symmetric")
-        else:
-            img_pad = img
-        img_mask = numpy.zeros_like(img_pad)
-        strel = skimage.morphology.disk(radius)
-        img_mask[xy_region[0]:xy_region[1], xy_region[2]:xy_region[3]] = strel
-        I_E = numpy.mean(img_pad[numpy.nonzero(img_mask)])
-        I_StD = numpy.std(img_pad[numpy.nonzero(img_mask)])
-        # to prevent low variance regions, compare I_StD to T_StD
-        I_StD = T_StD if I_StD < T_StD else I_StD
-        I_N = (img[pixel_location] - I_E) / I_StD
-        return I_N
-
-    def calculate_aln_image(self, img, radius_image, threshold_std):
+    def calculate_aln_image(self, img, radius_image, threshold_std, radius_max):
         """
         :param self: an instance of AdaptiveLocalNormalization class
         :param img: a 2D ndarray/image
         :param radius_image: a 2D ndarray the same size as img with search_radius for every pixel
         :param threshold_std: a float within [0,1.0). It specifies a fraction of the global_std.
+        :param radius_max: The largest radius allowed during the radius search for each pixel
         :return aln_image: img after normalization. A 2D ndarray the same size as img.
         """
-        T_StD = threshold_std * numpy.std(img)
-        aln_image = numpy.zeros_like(img)
-        indexiter = numpy.nditer(img, flags=['multi_index'])
-        while not indexiter.finished:
-            pixel_location = indexiter.multi_index
-            # do stuff zone
-            aln_pixel = self.aln_normalize(img, pixel_location, radius_image[pixel_location].astype("int"), T_StD)
-            aln_image[pixel_location] = aln_pixel
-            indexiter.iternext()
+        aln_image = numpy.zeros_like(img, dtype="float")
+        global_std = numpy.std(img)
+        global_mean = numpy.mean(img)
+        t_std = global_std * threshold_std
+        t_cv = t_std / global_mean
+        radii = numpy.arange(radius_max) + 1
+        for r in radii:
+            if self.strel_shape.value == "disc":
+                if self.radius_method.value == "std":
+                    strel = skimage.morphology.disk(r)
+                    img_std = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.std, footprint=strel)
+                    img_std[img_std < t_std] = t_std
+                    img_mean = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.mean, footprint=strel)
+                    img_norm = (img.astype("float") - img_mean) / img_std
+                    aln_image[radius_image == r] = img_norm[radius_image == r]
+                elif self.radius_method.value == "cv":
+                    strel = skimage.morphology.disk(r)
+                    img_std = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.std, footprint=strel)
+                    img_mean = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.mean, footprint=strel)
+                    img_std[img_std < t_std] = t_std
+                    img_norm = (img.astype("float") - img_mean) / img_std
+                    aln_image[radius_image == r] = img_norm[radius_image == r]
+            elif self.strel_shape.value == "square":
+                if self.radius_method.value == "std":
+                    img_std = self.uniform_filter_std(img.astype("float"), r)
+                    img_std[img_std < t_std] = t_std
+                    img_mean = self.uniform_filter_mean(img.astype("float"), r)
+                    img_norm = (img.astype("float") - img_mean) / img_std
+                    aln_image[radius_image == r] = img_norm[radius_image == r]
+                elif self.radius_method.value == "cv":
+                    img_std = self.uniform_filter_std(img.astype("float"), r)
+                    img_mean = self.uniform_filter_mean(img.astype("float"), r)
+                    img_std[img_std < t_std] = t_std
+                    img_norm = (img.astype("float") - img_mean) / img_std
+                    aln_image[radius_image == r] = img_norm[radius_image == r]
+        aln_image = skimage.exposure.rescale_intensity(aln_image, out_range = numpy.uint16)
+        aln_image = numpy.uint16(aln_image)
         return aln_image
 
-    def calculate_radius_image(self, img, threshold_std, radius_max_float):
+    def calculate_radius_image(self, img, threshold_std, radius_max):
         """
         :param self: an instance of AdaptiveLocalNormalization class
         :param img: a 2D ndarray/image
         :param threshold_std: a float within [0,1.0). It specifies a fraction of the global_std.
+        :param radius_max: The largest radius allowed during the radius search for each pixel
         :return radius_image: a 2D ndarray the same size as img with search_radius for every pixel
         """
-        radius_max = numpy.mean(img.shape).astype("int")
         radius_image = numpy.zeros_like(img)
         global_std = numpy.std(img)
-        indexiter = numpy.nditer(img, flags=['multi_index'])
-        while not indexiter.finished:
-            pixel_location = indexiter.multi_index
-            # do stuff zone
-            aln_radius = self.find_aln_radius(img, pixel_location, global_std, threshold_std, radius_max)
-            radius_image[pixel_location] = aln_radius
-            indexiter.iternext()
+        global_mean = numpy.mean(img)
+        t_std = global_std * threshold_std
+        t_cv = t_std / global_mean
+        radii = numpy.arange(radius_max) + 1
+        for r in radii:
+            if self.strel_shape.value == "disc":
+                if self.radius_method.value == "std":
+                    strel = skimage.morphology.disk(r)
+                    img_std = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.std, footprint=strel)
+                    img_bool = img_std <= t_std
+                    radius_image[img_bool] = r
+                elif self.radius_method.value == "cv":
+                    strel = skimage.morphology.disk(r)
+                    img_std = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.std, footprint=strel)
+                    img_mean = scipy.ndimage.filters.generic_filter(img.astype("float"), numpy.mean, footprint=strel)
+                    img_cv = img_std / (img_mean + 1) # the +1 is okay if uint8 or uint16
+                    img_bool = img_cv <= t_cv
+                    radius_image[img_bool] = r
+            elif self.strel_shape.value == "square":
+                if self.radius_method.value == "std":
+                    img_std = self.uniform_filter_std(img.astype("float"), r)
+                    img_bool = img_std <= t_std
+                    radius_image[img_bool] = r
+                elif self.radius_method.value == "cv":
+                    img_std = self.uniform_filter_std(img.astype("float"), r)
+                    img_mean = self.uniform_filter_mean(img.astype("float"), r)  # remove
+                    img_cv = img_std / (img_mean + 1) # the +1 is okay if uint8 or uint16
+                    img_bool = img_cv <= t_cv
+                    radius_image[img_bool] = r
         return radius_image
 
     def create_settings(self):
@@ -78,74 +121,82 @@ class AdaptiveLocalNormalization(cellprofiler.module.ImageProcessing):
         self.threshold_std_float = cellprofiler.setting.Float(
             "Standard Deviation Threshold", 0.5, doc="""
                     Enter a number within the range [0,1.0).
-                    This specifies a fraction of the standard deviation of the image.""")
+                    This specifies a fraction of the standard deviation of the image."""
+        )
 
-        self.radius_max_float = cellprofiler.setting.Float(
-            "Maximum Radius", 0.5, doc="""
-                    Enter a number within the range (0,1.0].
-                    This specifies the largest radius allowed during the radius search for each pixel.""")
+        self.radius_max = cellprofiler.setting.Integer(
+            "Maximum Radius", 25, doc="""
+                    Enter an integer greater than 0.
+                    This specifies the largest radius allowed during the radius search for each pixel.
+                    The larger the radius the longer the run time."""
+        )
 
-    def find_aln_radius(self, img, pixel_location, global_std, threshold_std, radius_max):
-        """
-        :param self: an instance of AdaptiveLocalNormalization class
-        :param img: a 2D ndarray/image
-        :param pixel_location: a tuple with 2 elements (row, col), coordinates in an ndarray
-        :param global_std: the standard deviation of img, pre-calculated for speed since this function will be called for every pixel
-        :param threshold_std: a float within [0,1.0). It specifies a fraction of the global_std.
-        :param radius_max: an integer that will stop the search when the radius reaches this number
-        :return search_radius: an integer > 0. The radius of a disc structuring element.
-        """
-        T_StD = global_std * threshold_std
-        # prime the while loop...
-        search_condition = True
-        # create mask
-        search_radius = 1
-        xy_region, pad_width = self.xy_region_and_pad_width(img.shape, pixel_location, search_radius)
-        if pad_width > 0:
-            img_pad = skimage.util.pad(img, pad_width, "symmetric")
-        else:
-            img_pad = img
-        img_mask = numpy.zeros(img_pad.shape)
-        strel = skimage.morphology.disk(search_radius)
-        img_mask[xy_region[0]:xy_region[1], xy_region[2]:xy_region[3]] = strel
-        #
-        while search_condition:
-            # calculate local std
-            search_std = numpy.std(img_pad[numpy.nonzero(img_mask)])
-            if search_std > T_StD:
-                search_condition = False
-                break
-            # search_std is < cutoff, so search continues
-            # create mask
-            search_radius = search_radius + 1
-            xy_region, pad_width = self.xy_region_and_pad_width(img.shape, pixel_location, search_radius)
-            if pad_width > 0:
-                img_pad = skimage.util.pad(img, pad_width, "symmetric")
-            else:
-                img_pad = img
-            img_mask = numpy.zeros(img_pad.shape)
-            strel = skimage.morphology.disk(search_radius)
-            img_mask[xy_region[0]:xy_region[1], xy_region[2]:xy_region[3]] = strel
-            # check for radius_max
-            if search_radius >= radius_max:
-                search_condition = False
-                break
-        return search_radius
+        self.radius_method = cellprofiler.setting.Choice(
+            "Radius Method", ["cv", "std"], doc="""
+                    *cv* uses the coefficient of variation to calculate the radius.
+                    *std* uses the standard deviation to calculate the radius.""",
+            value="std"
+        )
+
+        self.strel_shape = cellprofiler.setting.Choice(
+            "Structuring Element Shape", ["square", "disc"], doc="""
+                    *square* is approx. 1000 times faster than *disc*.
+                    *disc* is true to the source algorithm.""",
+            value="square"
+        )
 
     def settings(self):
         __settings__ = super(AdaptiveLocalNormalization, self).settings()
 
         return __settings__ + [
             self.threshold_std_float,
-            self.radius_max_float
+            self.radius_max,
+            self.radius_method,
+            self.strel_shape
         ]
+
+    def uniform_filter_std(self, img, window_size):
+        """
+        :param self: an instance of AdaptiveLocalNormalization class
+        :param img: a 2D ndarray/image
+        :param window_size: a square array is used to filter img. window_size defines the size of the array, e.g. (n,n)
+        :return img_std: the img after applying a standard deviation filter.
+
+        Reference:
+        https://nickc1.github.io/python,/matlab/2016/05/17/Standard-Deviation-(Filters)-in-Matlab-and-Python.html
+
+        The scipy function will return nan in unexpected ways, so they are removed before returning the img.
+        """
+        c1 = scipy.ndimage.filters.uniform_filter(img, window_size)
+        c2 = scipy.ndimage.filters.uniform_filter(numpy.multiply(img, img), window_size)
+        img_std = numpy.sqrt(c2 - numpy.multiply(c1, c1))
+        img_std[numpy.isnan(img_std)] = numpy.mean(img_std)
+        return img_std
+
+    def uniform_filter_mean(self, img, window_size):
+        """
+        :param self: an instance of AdaptiveLocalNormalization class
+        :param img: a 2D ndarray/image
+        :param window_size: a square array is used to filter img. window_size defines the size of the array, e.g. (n,n)
+        :return img_mean: the img after applying a mean filter.
+
+        Reference:
+        https://nickc1.github.io/python,/matlab/2016/05/17/Standard-Deviation-(Filters)-in-Matlab-and-Python.html
+
+        The scipy function will return nan in unexpected ways, so they are removed before returning the img.
+        """
+        img_mean = scipy.ndimage.filters.uniform_filter(img, window_size)
+        img_mean[numpy.isnan(img_mean)] = numpy.mean(img_mean)
+        return img_mean
 
     def visible_settings(self):
         __settings__ = super(AdaptiveLocalNormalization, self).settings()
 
         return __settings__ + [
             self.threshold_std_float,
-            self.radius_max_float
+            self.radius_max,
+            self.radius_method,
+            self.strel_shape
         ]
 
     def run(self, workspace):
@@ -157,15 +208,18 @@ class AdaptiveLocalNormalization(cellprofiler.module.ImageProcessing):
         # perform normalization slice by slice for z-stack
         y_data = numpy.zeros_like(x_data)
         # The if-clause determines if data is 2D or 3D
-        if len(x_data.shape) > 2:
+        if x.volumetric:
             for index, plane in enumerate(x_data):
+                print index
                 radius_image = self.calculate_radius_image(plane, self.threshold_std_float.value,
-                                                           self.radius_max_float.value)
-                y_data[index] = self.calculate_aln_image(plane, radius_image, self.threshold_std_float.value)
+                                                           self.radius_max.value)
+                y_data[index] = self.calculate_aln_image(plane, radius_image, self.threshold_std_float.value,
+                                                           self.radius_max.value)
         else:
             radius_image = self.calculate_radius_image(x_data, self.threshold_std_float.value,
-                                                       self.radius_max_float.value)
-            y_data = self.calculate_aln_image(x_data, radius_image, self.threshold_std_float.value)
+                                                       self.radius_max.value)
+            y_data = self.calculate_aln_image(x_data, radius_image, self.threshold_std_float.value,
+                                                       self.radius_max.value)
         y = cellprofiler.image.Image(
             dimensions=dimensions,
             image=y_data,
@@ -181,29 +235,5 @@ class AdaptiveLocalNormalization(cellprofiler.module.ImageProcessing):
 
             workspace.display_data.dimensions = dimensions
 
-    def xy_region_and_pad_width(self, img_shape, pixel_location, search_radius):
-        """
-        :param self: an instance of AdaptiveLocalNormalization class
-        :param img_shape: A tuple with 2 elements (rows, cols) of an ndarray/image
-        :param pixel_location: a tuple with 2 elements (row, col), coordinates in an ndarray
-        :param search_radius: an integer > 0. The radius of a disc structuring element.
-        :return xy_region: a tuple with 4 elements describing the range of rows and cols that holds the strel.
-        (row0,row1,col0,col1)
-        :return pad_width: the number of pixels required to pad an image, so that a strel can fit within it.
-        """
-        pad_width_width_upperleft = pixel_location[0] - search_radius
-        pad_width_height_upperleft = pixel_location[1] - search_radius
-        pad_width_upperleft = numpy.abs(numpy.amin([pad_width_width_upperleft, pad_width_height_upperleft]))
-        pad_width_width_lowerright = pixel_location[0] + search_radius + 1 - img_shape[0]
-        pad_width_height_lowerright = pixel_location[1] + search_radius + 1 - img_shape[1]
-        pad_width_lowerright = numpy.amax([pad_width_width_lowerright, pad_width_height_lowerright])
-        pad_width = numpy.amax([pad_width_upperleft, pad_width_lowerright])
-
-        xy_region = (pixel_location[0] - search_radius + pad_width,
-                     pixel_location[0] + search_radius + 1 + pad_width,
-                     pixel_location[1] - search_radius + pad_width,
-                     pixel_location[1] + search_radius + 1 + pad_width)
-
-        return xy_region, pad_width
 
 
